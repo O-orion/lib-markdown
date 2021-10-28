0f18eccc1e649df88d6482a07a3c154589b6ed36
Lucas Viana <lucas.lvtorres@gmail.com>
2021-10-28 18:11:46 -0400

    Jesus Cristo é o Senhor - Capturando texto

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-const chalck = require('chalk')
-const fs = require('fs')
+const chalck = require('chalk');
+const fs = require('fs');
 
-function capturaArquivo(caminhoArquivo){
-    const encoding = 'utf-8'
-    fs.readFile(caminhoArquivo,encoding, (_, texto) => {
-        console.log(chalck.green(texto))
-    })
+
+function extrairLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados= []
+    let temp;
+
+    while((temp = regex.exec(texto)) != null){
+        arrayResultados.push({
+            [temp[1]]:temp[2]
+        })
+    }
+
+    return arrayResultados;
 }
 
-capturaArquivo('./arquivos/texto.md')
-console.log(chalck.red("DEUS É BOM!!"))
+function tratamentoError(erro){
+    throw new Error(chalck.red(erro.code, 'Vixi, deu muito ruim irmão, só lamento, talvez o caminho esteja errado, fui'));
+}
+//asaync -> avisa ao Js que essa função é assicrona
+async function pegaArquivo(caminho){
+    //async await não muda a sintaxe
+    const encoding = 'utf-8';
+    try{ //tenta executar
+        const texto = await fs.promises.readFile(caminho, encoding); //await indica o que eu vou espera carrega
+        //vai executa primeiro tudo o que está ao seu lado direito pra depois joga na variavel
+        console.log(extrairLinks(texto))
+    }catch(erro){ //se tiver erro me avisa
+        tratamentoError(erro)
+    }
+
+
+
+}
+
+/* com then
+function pegandoArquivo(caminho){
+    const encoding = "utf-8"
+    fs.promises.readFile(caminho, encoding)
+                .then(texto => console.log(texto))
+                //encadeamento de funções
+                .catch(erro => tratamentoError(erro))
+}*/
+
+/*function capturaArquivo(caminhoArquivo){
+    const encoding = 'utf-8';
+    fs.readFile(caminhoArquivo,encoding, (erro, texto) => {
+        if(erro){
+            tratamentoError(erro);
+        }
+        console.log(chalck.green(texto));
+    })
+}
+*/
+pegaArquivo('./arquivos/texto1.md');
+
+
+
+console.log(chalck.red("DEUS É BOM!!"));
+
+
+
